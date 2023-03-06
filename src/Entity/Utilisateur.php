ce4f69bf4fa1b5ed767797d274166c813edeaf23
Baptiste DURET <bluebaptiste982@gmail.com>
2023-03-06 16:58:17 +0100

    first commit

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(length: 80)]
+    private ?string $MotDePasse = null;
+
+    #[ORM\Column(length: 15, nullable: true)]
+    private ?string $NumeroTelephone = null;
+
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $Email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getMotDePasse(): ?string
+    {
+        return $this->MotDePasse;
+    }
+
+    public function setMotDePasse(string $MotDePasse): self
+    {
+        $this->MotDePasse = $MotDePasse;
+
+        return $this;
+    }
+
+    public function getNumeroTelephone(): ?string
+    {
+        return $this->NumeroTelephone;
+    }
+
+    public function setNumeroTelephone(?string $NumeroTelephone): self
+    {
+        $this->NumeroTelephone = $NumeroTelephone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(?string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+}
